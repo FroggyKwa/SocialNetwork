49ecb5a34788bc16ebd41d1726075960f6995eea
FroggyKwa <ilya.laguxa@gmail.com>
2022-05-05 12:29:47 +1000

    fix static

--- SocialNetwork/settings.py
+++ SocialNetwork/settings.py
@@ -29,7 +29,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "social_network",
-    "rest_framework_simplejwt",
     "rest_framework",
 ]
 
@@ -116,7 +115,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "social_network/static/"
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- SocialNetwork/urls.py
+++ SocialNetwork/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("", include("social_network.urls")),
     path("admin/", admin.site.urls),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
